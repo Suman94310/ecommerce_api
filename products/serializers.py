858d94e6c0b15d1ad13f1f0c8b64a720fcf0f29e
Saurav <saurav@localhost.localdomain>
2021-03-02 02:26:58 +0530

    fixing reg token 3

--- products/serializers.py
+++ products/serializers.py
@@ -27,6 +27,14 @@ class RegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ['username', 'email', 'password']
 
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
